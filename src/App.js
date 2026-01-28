cf1061338530eefc8caee83f96d524e2549e7041
Prasanth <prasanthpower5p@gmail.com>
2026-01-28 14:35:12 +0530

    describe what you changed

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
-				<Route path="/articles" element={<Articles />} />
+
 				<Route path="/article/:slug" element={<ReadArticle />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="*" element={<Notfound />} />
