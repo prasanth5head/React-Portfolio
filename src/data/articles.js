c2bf07d26c00c24c2cd672c75e2bafa1ad067929
Prasanth <prasanthpower5p@gmail.com>
2026-01-28 14:49:02 +0530

    describe what you changekjjkd

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function article_1() {
 	return {
-		date: "7 May 2023",
+		date: "7 May 2025",
 		title: "The Benefits of Cloud Computing",
 		description:
 			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
@@ -41,7 +41,7 @@ function article_1() {
 
 function article_2() {
 	return {
-		date: "7 May 2023",
+		date: "7 May 2025",
 		title: "Artificial Intelligence in Healthcare",
 		description:
 			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
